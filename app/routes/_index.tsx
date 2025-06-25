3eae285bf282c7db2d40f31b51895d2fc69cf537
Nabeel Aejaz <nabeelaejaz@gmail.com>
2025-06-25 15:40:07 +0500

    Added IBM GoogleFont

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -5,14 +5,22 @@ import { Link } from '@remix-run/react';
 export const meta: MetaFunction = () => {
   return [
     { title: "The Creative Authority" },
-    { name: "description", content: "Pakistan’s Premium Shopify App Development & Design Agency" },
+    { name: "description", content: "Pakistans Premium Shopify App Development & Design Agency" },
   ];
 };
 
 export default function Index() {
   return (
     <div>
-      <h1>Hello World</h1>
+        <div className="ml-8 w-40 h-20">
+          <img src="tcalogo2025.svg" alt="The Creative Authority" />
+        </div>
+        <div className="ml-8 mt-12">
+          <img src="woman-stands.jpg" alt="Woman Stands with her arm folded in a store" />
+        </div>
+        <div>
+          <p className="ibm-plex-serif-semibold">Pakistans Premium Shopify App Development & Design Agency</p>
+        </div>
     </div>
   );
 };
