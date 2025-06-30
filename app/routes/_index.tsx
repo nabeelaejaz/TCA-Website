1a85f7982c25d1db2dce76bc697d0084e51069a2
Nabeel Aejaz <nabeelaejaz@gmail.com>
2025-06-30 17:22:00 +0500

    Added Links

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,6 +1,7 @@
 import type { MetaFunction } from "@remix-run/node";
 import TcaLogo from "~/components/tcalogo";
 import DotTCA from "~/components/dottca";
+import { Link } from "@remix-run/react";
 
 export const meta: MetaFunction = () => {
   return [
@@ -145,13 +146,13 @@ export default function Index() {
         </div>
 
         <div className="md:mt-20 mt-12 mb-12 justify-center flex">
-          <button className="bg-blue-800 text-white p-[50px] md:w-[460px] w-[300px] md:h-[240px] h-[200px]"><p className="mt-[-34px] md:ml-[-84px] ml-[-52px] ibm-plex-sans-semibold md:text-[38px] text-[26px]">GOT A PROJECT?</p><p className="md:ml-[-106px] ml-[-62px] md:mt-[-6px] mt-[-4px] ibm-plex-sans-regular md:text-[26px] text-[18px]">Speak With Our Experts</p><img className="size-14 md:mt-16 mt-14 md:ml-80 ml-44" src="arrow--right.svg" alt="Contact Us" /></button>
+          <Link to="/contact" className="bg-blue-800 text-white p-[50px] md:w-[460px] w-[300px] md:h-[240px] h-[200px]"><p className="mt-[-34px] md:ml-[-24px] ml-[-32px] ibm-plex-sans-semibold md:text-[38px] text-[26px]">GOT A PROJECT?</p><p className="md:ml-[-20px] ml-[-30px] md:mt-[-6px] mt-[-4px] ibm-plex-sans-regular md:text-[26px] text-[18px]">Speak With Our Experts</p><img className="size-14 md:mt-16 mt-14 md:ml-80 ml-44" src="arrow--right.svg" alt="Contact Us" /></Link>
         </div>
 
         <div className="md:mt-28 mb-20 flex ibm-plex-serif-semibold md:text-[18px]">
           <DotTCA />
-          <p className="md:ml-12 ml-8 mt-1">Careers</p>
-          <p className="md:ml-12 ml-8 mt-1">Contact Us</p>
+          <p className="md:ml-12 ml-8 mt-1"><Link to="/careers">Careers</Link></p>
+          <p className="md:ml-12 ml-8 mt-1"><Link to="/contact">Contact Us</Link></p>
         </div>
 
     </div>
