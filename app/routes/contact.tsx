64013e9b7aae261e290c94af204a73102da50368
Nabeel Aejaz <nabeelaejaz@gmail.com>
2025-07-09 16:31:10 +0500

    Testing Forms 14

--- app/routes/contact.tsx
+++ app/routes/contact.tsx
@@ -1,6 +1,31 @@
 import { Form } from "@remix-run/react";
 
 export default function Contact() {
+  const [result, setResult] = React.useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setResult("Sending....");
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "73f79c76-ec77-4ca3-a47e-f57248014056");
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      setResult("Form Submitted Successfully");
+      event.target.reset();
+    } else {
+      console.log("Error", data);
+      setResult(data.message);
+    }
+  };
+  
   return (
     <div className="md:flex md:flex-row-reverse lg:ml-8 mt-20">
       <div className="md:w-[50%]">
@@ -19,6 +44,7 @@ export default function Contact() {
               <button className="carbonbutton carbonreset" type="reset">Reset</button>
             </div>
           </form>
+          <span>{result}</span>
       </div>
     </div>
   );
